b12180cff559ee1a4ed9aa1afdf9e6de8d1785cf
jpwakugawa <jp_wakugawa@outlook.com>
2022-06-13 18:02:55 -0400

    chore: adicionando subitens crud no menu

--- Menu.java
+++ Menu.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 
 public class Menu extends JMenuBar {
 	JMenu cliente, pedido, pizza, formaDePagamento;
+	JMenuItem create, read, update, delete;
 	
 	Menu(){
 		cliente = new JMenu("Cliente");
@@ -9,7 +10,16 @@ public class Menu extends JMenuBar {
 		pizza = new JMenu("Pizza");
 		formaDePagamento = new JMenu("Forma de pagamento");
 		
-
+		create = new JMenuItem("Create");
+		read = new JMenuItem("Read");
+		update = new JMenuItem("Update");
+		delete = new JMenuItem("Delete");
+		
+		cliente.add(create);
+		cliente.add(read);
+		cliente.add(update);
+		cliente.add(delete);
+		
 		this.add(cliente);
 		this.add(pedido);
 		this.add(pizza);
