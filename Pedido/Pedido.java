2fd4010932ea1c9104913e7af517f26bea8123e2
jpwakugawa <jp_wakugawa@outlook.com>
2022-06-28 10:48:55 -0400

    chore: config inicial do Pedido

--- Pedido/Pedido.java
+++ Pedido/Pedido.java
@@ -1,35 +1,53 @@
-import Cliente.Cliente;
+package Pedido;
+import Cliente.*;
 import FormaDePagamento.*;
-import Pizza.Pizza;
+import Pizza.*;
 
 public class Pedido {
   private int id;
   private Pizza pizza;
   private Cliente cliente;
   private FormaDePagamento formaDePagamento;
-
+  
+  Pedido() {}
+  
+  Pedido(int id, Pizza pizza, Cliente cliente, FormaDePagamento formaDePagamento) {
+	  setId(id);
+	  setPizza(pizza);
+	  setCliente(cliente);
+	  setFormaDePagamento(formaDePagamento);
+  }
+  
   public int getId() {
   	return id;
   }
+  
   public void setId(int id) {
   	this.id = id;
   }
+  
   public Pizza getPizza() {
   	return pizza;
   }
+  
   public void setPizza(Pizza pizza) {
   	this.pizza = pizza;
   }
+  
   public Cliente getCliente() {
   	return cliente;
   }
+  
   public void setCliente(Cliente cliente) {
   	this.cliente = cliente;
   }
+  
   public FormaDePagamento getFormaDePagamento() {
   	return formaDePagamento;
   }
+  
   public void setFormaDePagamento(FormaDePagamento formaDePagamento) {
   	this.formaDePagamento = formaDePagamento;
   }
+  
 }
