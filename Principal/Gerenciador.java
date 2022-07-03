f703e00a5c15028c87c9dfdb9ecdd342946a2ff3
jpwakugawa <jp_wakugawa@outlook.com>
2022-07-02 20:46:08 -0400

    feat: adicionando listaDePizzas no Gerenciador

--- Principal/Gerenciador.java
+++ Principal/Gerenciador.java
@@ -1,12 +1,15 @@
 package Principal;
+
 import Cliente.*;
 import Pedido.*;
+import Pizza.*;
 import FormaDePagamento.*;
 import java.util.ArrayList;
 
 public class Gerenciador {
 	static ArrayList<Cliente> listaDeClientes;
 	static ArrayList<Pedido> listaDePedidos;
+	static ArrayList<Pizza> listaDePizzas;
 	static  ArrayList<FormaDePagamento> listaDeFormaDePagamentos;
 	
 	private Gerenciador() {}
@@ -23,6 +26,12 @@ public class Gerenciador {
 			listaDePedidos = new ArrayList<Pedido>();
 		}
 		return listaDePedidos;
+	}
+
+	public static ArrayList<Pizza> getListaDePizzas(){
+		if(listaDePizzas == null)
+			listaDePizzas = new ArrayList<Pizza>();
+		return listaDePizzas;
 	}
 	
 	public static ArrayList<FormaDePagamento> getListaDeFormasDePagamentos(){
